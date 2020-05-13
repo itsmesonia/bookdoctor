de88dd0c1206b8f632dc66c9535bb034466fe12a
Aichi Chang <phoebexxxc@gmail.com>
2020-05-13 22:09:39 +0100

    changed doctor model

--- Backend/models/doctor.js
+++ Backend/models/doctor.js
@@ -1,14 +1,25 @@
 const mongoose = require('mongoose')
-const uniqueValidatior = require('mongoose-unique-validator')
+const uniqueValidator = require('mongoose-unique-validator')
+
+
+const appointmentSchema = new mongoose.Schema({
+  date: { type: String, unique: true },
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+})
+
 
 const doctorSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   expertise: { type: String, required: true },
-
-  gp: { type: String, required: true }
+  gp: { type: String, required: true },
+  appointments: [ appointmentSchema ]
 })
 
-// doctorSchema.plugin(uniqueValidatior)
+
+appointmentSchema.plugin(uniqueValidator)
+
 
 module.exports = mongoose.model('Doctor', doctorSchema)
