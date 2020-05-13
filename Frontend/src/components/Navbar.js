1d1d286cf195f1e04ab0d0dcd81a4ca7e2e54821
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-05-13 14:55:15 +0100

    navbar and folder structure

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import 'bulma'
 
-const Navbar = () => (
-  <div>BUNA</div>
+const Navbar = () => {
 
-)
+  const [navClassName, setNavClassName] = React.useState('navbar-menu')
 
+  const clickBurger = () => {
+    if (navClassName === 'navbar-menu') {
+      setNavClassName('navbar-menu is-active')
+    } else {
+      setNavClassName('navbar-menu')
+    }
+  }
+
+  return (
+    <div className="navbar has-background-black">
+      {/* <div className="container"> */}
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">
+          <img className="logo" src=''/></Link>
+
+        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={clickBurger}>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+
+      </div>
+      <div className="navbar-menu is-active">
+        <div id="navbarBasicExample" className={navClassName}>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <Link className='white' to={'/'}>Home</Link>
+            </div>
+            <div className="navbar-item">
+              <Link className='white' to={'/about'}>About</Link>
+            </div>
+            <div className="navbar-item">
+              <Link className='white' to={'/login'}>Login</Link>
+            </div>
+            <div className="navbar-item">
+              <Link className='white'to={'/register'}>Register</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
+    </div>
+  )
+}
 
 export default Navbar
+
+
