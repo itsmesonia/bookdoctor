f7cb2b0218f6951ef69cc4fea0ea355c02814831
Aichi Chang <phoebexxxc@gmail.com>
2020-05-22 22:06:21 +0100

    login page completed

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -1,80 +1,105 @@
-import React from 'react'  
+import React, { useState } from 'react'  
 import Avatar from '@material-ui/core/Avatar'  
-import Button from '@material-ui/core/Button'  
 import CssBaseline from '@material-ui/core/CssBaseline'  
 import TextField from '@material-ui/core/TextField'  
-import FormControlLabel from '@material-ui/core/FormControlLabel'  
-import Checkbox from '@material-ui/core/Checkbox'  
-import Link from '@material-ui/core/Link'  
-// import Paper from '@material-ui/core/Paper'  
+import Link from '@material-ui/core/Link'   
 import Box from '@material-ui/core/Box'  
 import Grid from '@material-ui/core/Grid'  
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'  
-import Typography from '@material-ui/core/Typography'  
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn'  
 import { makeStyles } from '@material-ui/core/styles'  
+import Select from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
 
-function Login() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        bookdoctors.com
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  )  
-}
+import axios from 'axios'
+import Auth from '../lib/auth'
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100vh',
+    height: '100vh'
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundImage: 'url(https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
     backgroundSize: 'cover',
-    backgroundPosition: 'center',
+    backgroundPosition: 'center'
   },
   paper: {
     margin: theme.spacing(8, 4),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(3, 0, 2)
+  }
 }))  
 
-export default function SignInSide() {
+
+
+export default function Login(props) {
   const classes = useStyles()  
+
+  const [loginInfo, setLoginInfo] = useState({
+    email: '',
+    password: '',
+    role: ''
+  })
+  const [errors, setErrors] = useState('')
+
+
+  function handleChange(e) {
+    e.persist()
+    setLoginInfo({ ...loginInfo, [e.target.name]: e.target.value })
+  }
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios.post('/api/login', loginInfo)
+      .then(res => {
+        Auth.setToken(res.data.token)
+        Auth.setUser(res.data.user)
+        if (errors === '') {
+          props.history.push('/')
+          window.location.reload()
+        }
+        
+      })
+      .catch(err => setErrors(err.response.data))
+  }
+
 
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} elevation={6} square>
+      <Grid item xs={12} sm={8} md={5} elevation={6} square='true'>
         <div className={classes.paper}>
+
           <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
+            <AssignmentTurnedInIcon /> 
           </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
+          
+          <h1>Login</h1>
+
+          <form className='formStyle' onSubmit={(e) => handleSubmit(e)}>
+            
             <TextField
-              // variant="outlined"
+              onChange={(e) => handleChange(e)}
+              variant="outlined"
               margin="normal"
               required
               fullWidth
@@ -84,8 +109,10 @@ export default function SignInSide() {
               autoComplete="email"
               autoFocus
             />
+            {/* {errors.username && <small>{errors.username.message}</small>} */}
             <TextField
-              // variant="outlined"
+              onChange={(e) => handleChange(e)}
+              variant="outlined"
               margin="normal"
               required
               fullWidth
@@ -93,37 +120,40 @@ export default function SignInSide() {
               label="Password"
               type="password"
               id="password"
-              autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
+            <FormControl variant="outlined" >
+              <InputLabel className='selectInput' htmlFor="outlined-age-native-simple">Login as</InputLabel>
+              <Select
+                className='selectInput'
+                native
+                onChange={(e) => handleChange(e)}
+                label='login as..'
+                name='role'
+                // inputProps={{
+                //   name: 'role'
+                //   // id: 'outlined-age-native-simple'
+                // }}
+              >
+                <option aria-label="None" value="" />
+                <option value='patient'>Patient</option>
+                <option value='doctor'>Doctor</option>
+              </Select>
+            </FormControl>
+            
+            
+
+            <button className='button' >
               Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}>
-              <Login />
-            </Box>
+            </button>
+
           </form>
+            
+          <Link to="/login" className='links'>Already have an account? Login</Link>
+
+          <Box mt={5}>
+            <div>Copyright © <Link target='blank' className='links' to="https://github.com/soniacweb/bookdoctor">bookdoctors.com</Link>{' '}{new Date().getFullYear()}</div>
+          </Box>
+
         </div>
       </Grid>
     </Grid>
