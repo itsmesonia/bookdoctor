d081871a7902bb18727dd006bc8e63f224f07b10
Aichi Chang <phoebexxxc@gmail.com>
2020-05-26 13:27:23 +0100

    fixed sass to css compiler

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -43,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: '#AE2573'
   },
   form: {
     width: '100%', // Fix IE 11 issue.
