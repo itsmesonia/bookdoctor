19552356b152726b69f76ccb7524948dd5f57874
Aichi Chang <phoebexxxc@gmail.com>
2020-05-27 20:45:02 +0100

    change date to string at appointment

--- Backend/models/appointment.js
+++ Backend/models/appointment.js
@@ -4,7 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator')
 const Schema = mongoose.Schema
 
 const appointmentSchema = new Schema({
-  date: { type: Date, required: true },
+  date: { type: String, required: true },
   reason: { type: String, required: true },
   doctor: { type: String, require: true },
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
