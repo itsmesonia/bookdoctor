3122f44704e1936aa8331268dbeabc9e7a73c2b5
Aichi Chang <phoebexxxc@gmail.com>
2020-06-02 14:23:49 +0100

    fixed bug

--- Backend/config/environment.js
+++ Backend/config/environment.js
@@ -5,11 +5,9 @@ const dbURI = `${dbURIPrefix}${dbName}`
 // const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/bookdoctor'
 // our secret used for encoding our JWT tokens, used in '/controllers/user' and '/lib/secureRoute'
 const secret = 'This is my really secret string that nobody is going to be able to guess1'
-const ASENDGRID_API_KEY = 'SG.hsgqwSSpSs2s918-mHXsaA.EUG9ZUxTgtZXZudNOZT8nta2XqaOQsF17QOmoX8GJtU'
 
 module.exports = { 
   port, 
   dbURI, 
-  secret,
-  ASENDGRID_API_KEY 
+  secret
 }
