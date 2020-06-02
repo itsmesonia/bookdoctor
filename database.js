e76016fb89009ed355c1d508cccbd0b9d5b03fc4
Aichi Chang <phoebexxxc@gmail.com>
2020-06-02 21:23:24 +0100

    fix git

--- database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-const connection = 'mongodb+srv://aichichang:bookDoctor2020@cluster369-aprqf.mongodb.net/book-doctor?retryWrites=true&w=majority'
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-  .then(() => console.log('Database Connected Successfully'))
-  .catch(err => console.log(err))
