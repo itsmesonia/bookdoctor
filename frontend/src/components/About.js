aaca212ec13095a90acf65b197acb1c449f736f6
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-06-03 19:25:40 +0100

    api update in abouts page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -28,7 +28,7 @@ export default function About() {
         opacity={0.6}
         minHeight={'140vh'}
         style={{ overflow: 'hidden' }}>
-        <h1 className='title'>Who We Are</h1>
+        <h1 className='title'>Stay Home | Save Lives</h1>
         <p className='content'>Find out more on how to keep yourself and your family safe</p>
       </LazyHero>
 
@@ -38,36 +38,67 @@ export default function About() {
             <div className="tile is-parent is-vertical">
               <article className="tile is-child notification">
                 <p className="title">
-                  Who we are.
-                  {article.headline}</p>
+                  Block 1: Who we are.</p>
+
                 {/* <p className="subtitle">{article.hasPart[0].description}</p> */}
+
               </article>
               <article className="tile is-child notification">
+                <p className="title">Testing</p>
+                {/* <p className="sub-title"> {article.mainEntityOfPage[1] && article.mainEntityOfPage[1].text}</p> */}
+                {/* <p className="sub-title"> {article[1]. && article.mainEntityOfPage[1].text}</p> */}
+                
+                {/* <p>{article.mainEntityOfPage[0].mainEntityOfPage.text && article.mainEntityOfPage[0].mainEntityOfPage.text}</p> */}
+                
+                {/* <p>
+                  {article.mainEntityOfpage[0].mainEntityOfPage[0] && article.mainEntityOfPage[0].mainEntityOfPage[0].map((info, i)=> {
+                    return <p key={i}>{info.text}</p> 
+                  }
+                  )}
+                </p> */}
+
+                {/* <p>
+                  {article.mainEntityOfPage[1] && article.mainEntityOfPage[1].map((info, i)=> {
+                    return <p key={i}>{info.text}</p> 
+                  }
+                  )}
+                </p> */}
+
                 <p className="title">Where can I find more information?</p>
-                <p className="sub-title">{article.name}</p>
-                <p>For more information, please check out our</p><a href={article.url}>NHS Resources here</a>
+                {/* <p className="sub-title">{article.name}</p> */}
+                <p className="sub-title">{article.description}</p>
+                <p>For more information, please check out our</p><a href={article.url} target="_blank" rel="noreferrer">NHS Resources here</a>
                 {/* <p className="subtitle">{article.about}</p> */}
               </article>
             </div>
             <div className="tile is-parent">
               <article className="tile is-child notification">
                 <p className="title">
-                  {/* {state.article.headline} */}
-                
+                  The Importance Of Selfcare
                 </p>
-                {/* <p className="subtitle">{article.name}</p> */}
+                <div className="content">
+                  <p className="sub-title"> {article.hasPart && article.hasPart[3].text.replace(/<[^>]*>/g, ' ')}</p>
+                  <p className="sub-title"> <a href={article.hasPart && article.hasPart[3].url} target="_blank" rel="noreferrer">Find out more on Selfcare</a></p>
+                </div>
+
                 <figure className="image is-4by7">
                   <img src="https://i.imgur.com/KbkSHZJ.jpg" />
                 </figure>
               </article>
             </div>
           </div>
+         
           <div className="tile is-parent">
             <article className="tile is-child notification">
-              {/* <p className="title">{article.name}</p>
-              <p className="subtitle">{article.name}</p> */}
+              <p className="title">Symptoms</p>
+
+              {/* //trying to access the second haspart object */}
               <div className="content">
-          content
+              
+                {/* <p className="sub-title">{article.hasPart && article.hasPart[1].description}</p> */}
+                <p className="subtitle">{article.hasPart && article.hasPart[1].text.replace(/<[^>]*>/g, ' ')}</p>
+                <p a href={article.hasPart && article.hasPart[1].url} target="_blank" rel="noreferrer">More info here</p>
+
               </div>
             </article>
           </div>
@@ -75,39 +106,45 @@ export default function About() {
         <div className="tile is-parent">
           <article className="tile is-child notification">
             <div className="content">
-              <p className="title">works fine {article.headline}</p>
-              <p className="subtitle"> works fine {article.description}</p>
+              <p className="title">What is Corona?</p>
+              <p className="sub-title">{article.headline}</p>
+              {/* <p className="subtitle">{article.description}</p> */}
               <div className="content">
                 {article.text}
-                {/* {article.mainEntityOfPage.mainEntityOfPage[1]}
-                {article.mainEntityOfPage.mainEntityOfPage[2]}
-                {article.mainEntityOfPage.mainEntityOfPage[3]}
-                {article.mainEntityOfPage.mainEntityOfPage[3]} */}
 
-                {/* stackoverflow solution to array of objects: 
-                <ul>Quote: { response.map((item, index) => (<li key={index}>{item.quote}</li>)) }</ul> */}
+                <p>
+                  {article.itemListElement && article.itemListElement.map((info, i)=> {
+                    return <p key={i}>{info.name}</p> 
+                  }
+                  )}
+                </p>
 
-                {/* <ul> { article.map((item, index) => (
-                  <li key={index}>{item.hasPart}</li>)) }
-                </ul> */}
 
-                {/* Aichi's solution: */}
+                <p>
+                  {article.hasPart && article.hasPart.map((info, i)=> {
+                    return <p key={i}>{info.description}</p> 
+                  }
+                  )}
+                </p>
+
+
+                {/* full article works more information on covid: */}
                 {/* <p>
-                  {article.hasPart.map((info, i)=> {
-                    return <div key={i}>{info.description}</div> 
+                  {article.hasPart && article.hasPart.map((info, i)=> {
+                    return <p key={i}>{info.text.replace(/<[^>]*>/g, '')}</p> 
                   }
                   )}
                 </p> */}
+                
+                <p className="title">Treatments</p>
+                {/* <p className="sub-title"> {article.hasPart && article.hasPart[4].description}</p> */}
+                <p className="sub-title"> {article.hasPart && article.hasPart[4].text.replace(/<[^>]*>/g, ' ')}</p>
+                <p className="title">Prevention</p>
+                <p className="sub-title"> {article.hasPart && article.hasPart[5].description.replace(/<[^>]*>/g, ' ')}</p>
+                <p className="sub-title"> {article.hasPart && article.hasPart[5].text.replace(/<[^>]*>/g, ' ')}</p>
+                <p>NHS guidelines for more information, on prevention </p><a href={article.hasPart && article.hasPart[5].url} target="_blank" rel="noreferrer">here</a>
 
-                {/* <p> {article.about}</p> */}
 
-                {/* <div> 
-                  {article[1].hasPart.map((value, i) => {
-                    return <p key={i}>{value}</p>
-                  })}
-                </div>  */}
-
-                {/* <p>{article[1].hasPart[1]}</p> */}
               </div>
             </div>
           </article>
