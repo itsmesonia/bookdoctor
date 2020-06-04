f8a76f95ffe4afad48d1195a244cdc4c2a649df9
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-06-04 02:09:48 +0100

    updated stats from second api

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -6,6 +6,8 @@ import 'bulma'
 import axios from 'axios'
 // import { Link } from 'react-router-dom'
 
+import AboutStats from './AboutStats' 
+
 export default function About() {
 
   const [article, setArticle] = useState('')
@@ -36,23 +38,52 @@ export default function About() {
         <div className="tile is-vertical is-8">
           <div className="tile">
             <div className="tile is-parent is-vertical">
-              <article className="tile is-child notification">
-                <p className="title">
-                  Block 1: Who we are.</p>
 
-                {/* <p className="subtitle">{article.hasPart[0].description}</p> */}
+              <article className="tile is-child notification">
+                <p className="title">Coronavirus Explained</p>
+                <p className="subtitle">{article.headline}</p>
+                <p className="subtitle">{article.text}</p>
+
+                <p>
+                  {article.itemListElement && article.itemListElement.map((info, i)=> {
+                    return <p key={i}>{info.name}</p> 
+                  }
+                  )}
+                </p>
+
+                <p>
+                  {article.hasPart && article.hasPart.map((info, i)=> {
+                    return <p key={i}>{info.description}</p> 
+                  }
+                  )}
+                </p>
+
+                {/* <figure className="image is-4by3">
+                  <img src="https://i.imgur.com/6WnBISG.jpg" />
+                </figure> */}
 
               </article>
+
+
               <article className="tile is-child notification">
-                <p className="title">Testing</p>
-                {/* <p className="sub-title"> {article.mainEntityOfPage[1] && article.mainEntityOfPage[1].text}</p> */}
-                {/* <p className="sub-title"> {article[1]. && article.mainEntityOfPage[1].text}</p> */}
-                
+                <p className="subtitle">Can I Get Tested for Coronavirus? Testing and tracing</p>
+                <p className="subtitle">Self-isolation and treatment if you have symptoms</p>
+                <p className="subtitle">GOV.UK: coronavirus advice for traveller- should I tell my GP Im going away?</p>
+                <p className="subtitle">GOV.UK: coronavirus action plan</p>
+                <p className="subtitle">Government information and advice</p>
+                <p className="subtitle">People at higher risk</p>
+                <p className="subtitle">How to check if your child has symptoms</p>
+                <p className="subtitle">Social distancing and changes to everyday lif</p>
+                {/* new block for doctors? */}
+                <p className="subtitle">Information For Health Professionals</p>
+     
                 {/* <p>{article.mainEntityOfPage[0].mainEntityOfPage.text && article.mainEntityOfPage[0].mainEntityOfPage.text}</p> */}
                 
+                {/* <p>{article.mainEntityOfpage && article.mainEntityOfPage[0].mainEntityOfPage[0].text}</p> */}
+
                 {/* <p>
-                  {article.mainEntityOfpage[0].mainEntityOfPage[0] && article.mainEntityOfPage[0].mainEntityOfPage[0].map((info, i)=> {
-                    return <p key={i}>{info.text}</p> 
+                  {article.mainEntityOfpage && article.mainEntityOfPage[0].mainEntityOfPage.map((info, i)=> {
+                    return <p key={i}>{info.text}</p>
                   }
                   )}
                 </p> */}
@@ -64,21 +95,20 @@ export default function About() {
                   )}
                 </p> */}
 
-                <p className="title">Where can I find more information?</p>
-                {/* <p className="sub-title">{article.name}</p> */}
-                <p className="sub-title">{article.description}</p>
+                <p className="subtitle">Where can I find more information?</p>
+                <p className="subtitle">{article.description}</p>
                 <p>For more information, please check out our</p><a href={article.url} target="_blank" rel="noreferrer">NHS Resources here</a>
-                {/* <p className="subtitle">{article.about}</p> */}
               </article>
             </div>
+
             <div className="tile is-parent">
               <article className="tile is-child notification">
                 <p className="title">
-                  The Importance Of Selfcare
+                  The Importance of Selfcare
                 </p>
                 <div className="content">
-                  <p className="sub-title"> {article.hasPart && article.hasPart[3].text.replace(/<[^>]*>/g, ' ')}</p>
-                  <p className="sub-title"> <a href={article.hasPart && article.hasPart[3].url} target="_blank" rel="noreferrer">Find out more on Selfcare</a></p>
+                  <p className="subtitle"> {article.hasPart && article.hasPart[3].text.replace(/<[^>]*>/g, ' ')}</p>
+                  <p className="subtitle"> <a href={article.hasPart && article.hasPart[3].url} target="_blank" rel="noreferrer">Find out more on Selfcare</a></p>
                 </div>
 
                 <figure className="image is-4by7">
@@ -95,39 +125,31 @@ export default function About() {
               {/* //trying to access the second haspart object */}
               <div className="content">
               
-                {/* <p className="sub-title">{article.hasPart && article.hasPart[1].description}</p> */}
+                {/* <p className="subtitle">{article.hasPart && article.hasPart[1].description}</p> */}
                 <p className="subtitle">{article.hasPart && article.hasPart[1].text.replace(/<[^>]*>/g, ' ')}</p>
-                <p a href={article.hasPart && article.hasPart[1].url} target="_blank" rel="noreferrer">More info here</p>
-
+                
+                <p><a href={article.hasPart && article.hasPart[1].url} target="_blank" rel="noreferrer">More info here</a></p>
               </div>
+             
+              {/* <figure className="image is-4by3">
+                <img src="https://i.imgur.com/aOroNmZ.jpg" />
+              </figure> */}
+    
             </article>
           </div>
         </div>
         <div className="tile is-parent">
           <article className="tile is-child notification">
             <div className="content">
-              <p className="title">What is Corona?</p>
-              <p className="sub-title">{article.headline}</p>
-              {/* <p className="subtitle">{article.description}</p> */}
+              
+              <p className="title">
+                  Live UK Stats</p>
+
+              <AboutStats />
+                
+        
               <div className="content">
-                {article.text}
-
-                <p>
-                  {article.itemListElement && article.itemListElement.map((info, i)=> {
-                    return <p key={i}>{info.name}</p> 
-                  }
-                  )}
-                </p>
-
-
-                <p>
-                  {article.hasPart && article.hasPart.map((info, i)=> {
-                    return <p key={i}>{info.description}</p> 
-                  }
-                  )}
-                </p>
-
-
+        
                 {/* full article works more information on covid: */}
                 {/* <p>
                   {article.hasPart && article.hasPart.map((info, i)=> {
@@ -135,15 +157,15 @@ export default function About() {
                   }
                   )}
                 </p> */}
-                
-                <p className="title">Treatments</p>
-                {/* <p className="sub-title"> {article.hasPart && article.hasPart[4].description}</p> */}
-                <p className="sub-title"> {article.hasPart && article.hasPart[4].text.replace(/<[^>]*>/g, ' ')}</p>
+               
+                         
                 <p className="title">Prevention</p>
-                <p className="sub-title"> {article.hasPart && article.hasPart[5].description.replace(/<[^>]*>/g, ' ')}</p>
-                <p className="sub-title"> {article.hasPart && article.hasPart[5].text.replace(/<[^>]*>/g, ' ')}</p>
+                <p className="subtitle"> {article.hasPart && article.hasPart[5].description.replace(/<[^>]*>/g, ' ')}</p>
+                <p className="subtitle"> {article.hasPart && article.hasPart[5].text.replace(/<[^>]*>/g, ' ')}</p>
                 <p>NHS guidelines for more information, on prevention </p><a href={article.hasPart && article.hasPart[5].url} target="_blank" rel="noreferrer">here</a>
 
+                <p className="title">Treatments</p>
+                <p className="subtitle"> {article.hasPart && article.hasPart[4].text.replace(/<[^>]*>/g, ' ')}</p>
 
               </div>
             </div>
---
+++ frontend/src/components/AboutStats.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import 'bulma'
+import axios from 'axios'
+import * as moment from 'moment'
+
+export default function AboutStats() {
+
+  const [stat, setStat] = useState('')
+
+  
+  useEffect(() => {
+    axios.get('https://api.covid19api.com/summary')
+      .then(res => setStat(res.data))
+  }, []) 
+    
+  
+  console.log(stat)
+
+  return (
+    <div>
+    
+      {/* <div className="tile is-ancestor">
+        <div className="tile is-vertical is-8">
+          <div className="tile">
+            <div className="tile is-parent is-vertical"> */}
+      {/* <article className="tile is-child notification">
+        <div className="notification is-info"> */}
+      {/* <button className="delete"></button> */}
+                
+      {/* <p className="title">Live UK Stats</p>  */}
+      <p className="sub-title-stats"> {stat.Countries && stat.Countries[176].Country}</p>
+      <p className="sub-title-stats"> Confirmed Cases: {stat.Countries && stat.Countries[176].NewConfirmed}</p>
+      <p className="sub-title-stats"> Total Confirmed: {stat.Countries && stat.Countries[176].TotalConfirmed}</p>
+      <p className="sub-title-stats"> New Death Toll: {stat.Countries && stat.Countries[176].NewDeaths}</p>
+      <p className="sub-title-stats"> Cumulative Lives Lost: {stat.Countries && stat.Countries[176].TotalDeaths}</p>
+      <p className="sub-title-stats"> Newly Recovered: {stat.Countries && stat.Countries[176].NewRecovered}</p>
+      <p className="sub-title-stats"> Total Recovered: {stat.Countries && stat.Countries[176].TotalRecovered}</p>      
+     
+      <p className="title" id="globalstats">
+                 Global Stats</p>
+
+      <p className="sub-title-stats"> Newly Recovered: {stat.Global && stat.Global.NewConfirmed}</p>
+      <p className="sub-title-stats"> New Confirmed Cases: {stat.Global && stat.Global.TotalConfirmed}</p>
+      <p className="sub-title-stats"> New Death Toll: {stat.Global && stat.Global.NewDeaths}</p>
+      <p className="sub-title-stats"> Cumulative Lives Lost Globally: {stat.Global && stat.Global.TotalDeaths}</p>
+      <p className="sub-title-stats"> Newly Recovered: {stat.Global && stat.Global.NewRecovered}</p>
+      <p className="sub-title-stats"> Total Recovered: {stat.Global && stat.Global.TotalRecovered}</p>
+
+      {/* reversing toISOString()format */}
+      <p id="dateupdated" className="sub-title-stats"> Updated as of {stat.Countries && stat.Countries[176].Date.replace(/T.*/,'').split('-').reverse().join('-')}</p> 
+
+                 
+    </div>
+    //   </article>
+    // </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+  )
+}
