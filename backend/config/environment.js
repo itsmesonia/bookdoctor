9a711a05af84c79ad61c40293e491b5b2fd82aa8
Aichi Chang <phoebexxxc@gmail.com>
2020-06-05 13:10:31 +0100

    Ready for deployment

---
+++ backend/config/environment.js
@@ -0,0 +1,13 @@
+const port = process.env.PORT || 8000
+const dbURIPrefix = 'mongodb://localhost/'
+const dbName = 'book-doctor'
+// const dbURI = `${dbURIPrefix}${dbName}`
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://aichichang:bookDoctor2020@cluster369-aprqf.mongodb.net/book-doctor?retryWrites=true&w=majority'
+// const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/bookdoctor'
+const secret = 'This is my really secret string that nobody is going to be able to guess1'
+
+module.exports = { 
+  port, 
+  dbURI, 
+  secret
+}
